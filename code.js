b57b545a02518f7cef98a156672c4aebdc2099a8
Foxtom91 <tommaso.varvello@gmail.com>
2023-05-08 17:43:51 +0100

    Write player choice and play round functions

--- code.js
+++ code.js
@@ -2,12 +2,49 @@ function getComputerChoice() {
     let number = Math.floor(Math.random() * 3)
     switch (number) {
         case 0:
-            return "Rock";
+            computerselection = "rock";
+            return true;
         case 1: 
-            return "Paper";
+            computerselection = 'paper';
+            return true;
         case 2:
-            return "Scissors";
+            computerselection = 'scissors';
+            return true;
     }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    playerselection = prompt("Please enter your choice: Rock, Paper or Scissors?").toLowerCase();
+}
+
+function playRound(a, b) {
+    if (a === 'rock' && b === 'scissors') {
+        return 1;
+    } else if (a === 'rock' && b === 'paper') {
+        return 2;
+    } else if (a === 'paper' && b === 'rock') { 
+        return 1;
+    } else if (a === 'paper' && b === 'scissors') {
+        return 2;
+    } else if (a === 'scissors' && b === 'paper') {
+        return 1;
+    } else if (a === 'scissors' && b === 'rock') {
+        return 2;
+    } else return 0;
+}
+
+let playerselection = '';
+let computerselection = '';
+getComputerChoice();
+getPlayerChoice();
+switch (playRound(playerselection, computerselection)) {
+    case 1:
+        console.log("Player wins!");
+        break;
+    case 2:
+        console.log("Computer wins!");
+        break;
+    case 0:
+        console.log("Draw!");
+        break;
+}
